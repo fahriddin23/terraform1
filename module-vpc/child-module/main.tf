e6a01120d0b0c67950f49e7022f0e28dc3ad492b
fahriddin <fahriddin24@gmail.com>
2023-06-29 17:08:07 -0500

    added new module to create vpc

---
+++ module-vpc/child-module/main.tf
@@ -0,0 +1,70 @@
+resource "aws_vpc" "my_vpc" {
+  cidr_block = var.vpc_cidr_block
+
+  tags = local.common_tags
+}
+
+
+resource "aws_subnet" "public_subnet" {
+  count                   = length(var.public_subnet_cidr_blocks)
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = var.public_subnet_cidr_blocks[count.index]
+  availability_zone       = local.availability_zone
+  map_public_ip_on_launch = true
+
+  tags = local.common_tags
+}
+
+
+resource "aws_subnet" "private_subnet" {
+  count                   = length(var.private_subnet_cidr_blocks)
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = var.private_subnet_cidr_blocks[count.index]
+  availability_zone       = local.availability_zone
+  map_public_ip_on_launch = false
+
+  tags = local.common_tags
+}
+
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = local.common_tags
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = local.common_tags
+}
+
+resource "aws_route_table_association" "public_subnet_association" {
+  count          = length(var.public_subnet_cidr_blocks)
+  subnet_id      = aws_subnet.public_subnet[count.index].id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_eip" "my_eip" {
+  vpc = true
+
+  tags = local.common_tags
+}
+
+resource "aws_nat_gateway" "my_nat_gateway" {
+  allocation_id = aws_eip.my_eip.id
+  subnet_id     = aws_subnet.public_subnet[0].id  # Choose any public subnet
+
+    tags = local.common_tags
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = local.common_tags
+}
+
+resource "aws_route_table_association" "private_subnet_association" {
+  count          = length(var.private_subnet_cidr_blocks)
+  subnet_id      = aws_subnet.private_subnet[count.index].id
+  route_table_id = aws_route_table.private_route_table.id
+}
